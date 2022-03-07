d51d57e6773627ecbfe396bfafcfce387174c741
Bongjin Choi <cbjjzzang@naver.com>
2022-03-08 08:47:24 +0900

    회원가입 페이지 version1

--- app.py
+++ app.py
@@ -1,12 +1,59 @@
 from pymongo import MongoClient
 
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.7fswg.mongodb.net/?retryWrites=true&w=majority')
-db = client.mini1
+db = client.sign_up
 
 
-#########################################################
-#봉진님
-#########################################################
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    useremail_receive = request.form['useremail_give']
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": useremail_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    useremail_receive = request.form['useremail_give']
+    exists = bool(db.users.find_one({"useremail": useremail_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/sign_up/check_dup2', methods=['POST'])
+def check_dup2():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
 
 
 
