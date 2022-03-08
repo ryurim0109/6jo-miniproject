a65f1ef662ee468a6c3d3d50f74cb2e9e2703f04
yurim <dbfla5036@naver.com>
2022-03-08 21:39:29 +0900

    커밋

--- app.py
+++ app.py
@@ -68,10 +68,7 @@ def check_dup2():
 
 
 #########################################################
-#예령님 더보기 버튼
-
-
-
+#예령님 더보기 버튼 구현이 안됨..
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
